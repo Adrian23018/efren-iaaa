36c870703c0a0beea1e460455c0aba4c3cf311fd
Juan Gonzalez <juangon.t@gmail.com>
2025-04-21 23:54:42 -0500

    feature/files add button search and files init

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,24 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { MenuModule } from 'primeng/menu';
 import { AvatarModule } from 'primeng/avatar';
+import { MenuItem } from 'primeng/api';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 
 @Component({
   selector: 'app-header',
   imports: [
     CommonModule,
-    ButtonModule,
     InputTextModule,
-    OverlayPanelModule,
+    ButtonModule,
     MenuModule,
-    AvatarModule
+    AvatarModule,
+    OverlayPanelModule,
+    InputGroupModule,
+    InputGroupAddonModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  items: MenuItem[] = [];
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Mi cuenta',
+        icon: 'pi pi-user',
+        command: () => {
+          console.log('Mi cuenta');
+        }
+      },
+      {
+        label: 'Ajustes',
+        icon: 'pi pi-cog',
+        command: () => {
+          console.log('Ajustes');
+        }
+      },
+      { separator: true },
+      {
+        label: 'Cerrar sesión',
+        icon: 'pi pi-sign-out',
+        command: () => {
+          console.log('Cerrar sesión');
+        }
+      }
+    ];
+  }
 }
