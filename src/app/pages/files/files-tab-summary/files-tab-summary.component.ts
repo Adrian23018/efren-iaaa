df20b25cbe7ae7b074fc2a10b9ee8368acfe99ec
Juan Gonzalez <juangon.t@gmail.com>
2025-04-23 13:01:38 -0500

    feature/files-insights add insights

--- src/app/pages/files/files-tab-summary/files-tab-summary.component.ts
+++ src/app/pages/files/files-tab-summary/files-tab-summary.component.ts
@@ -3,12 +3,14 @@ import { Component, Input } from '@angular/core';
 import { UserFile } from '@app/interfaces/files.model';
 import { Severity, TagUtil } from '@app/shared/utils/tagUtil';
 import { TagModule } from 'primeng/tag';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 @Component({
   selector: 'app-files-tab-summary',
   imports: [
     CommonModule,
     TagModule,
+    ScrollPanelModule,
   ],
   templateUrl: './files-tab-summary.component.html',
   styleUrl: './files-tab-summary.component.scss'
